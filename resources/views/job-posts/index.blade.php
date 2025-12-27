c8e367cb1a9ce72e92224e63eebb7e6b155209fa
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-28 01:55:27 +0630

    breadcrumb and easy navigation

--- resources/views/job-posts/index.blade.php
+++ resources/views/job-posts/index.blade.php
@@ -1,4 +1,5 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('job-posts.index')]" />
     @foreach ($jobPosts as $jobPost)
         <x-job-card class="mb-4" :$jobPost>
             <div>
--- resources/views/job-posts/show.blade.php
+++ resources/views/job-posts/show.blade.php
@@ -1,3 +1,4 @@
 <x-layout>
+    <x-breadcrumbs class="mb-4" :links="['Jobs' => route('job-posts.index'), $jobPost->title => '#']" />
     <x-job-card :$jobPost />
 </x-layout>
