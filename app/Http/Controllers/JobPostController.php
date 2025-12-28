dc2a498fcf9edb449467661d2cbce91bb8519e66
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-28 16:50:04 +0630

    radio button to filter experience level

--- app/Http/Controllers/JobPostController.php
+++ app/Http/Controllers/JobPostController.php
@@ -15,8 +15,16 @@ class JobPostController extends Controller
         $jobPosts = JobPost::query();
 
         $jobPosts->when(request('search'), function ($query) {
-            $query->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('description', 'like', '%' . request('search') . '%');
+            $query->where(function ($query) {
+                $query->where('title', 'like', '%' . request('search') . '%')
+                    ->orWhere('description', 'like', '%' . request('search') . '%');
+            });
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
+        })->when(request('experience'), function ($query) {
+            $query->where('experience', request('experience'));
         });
 
         return view('job-posts.index', ['jobPosts' => $jobPosts->get()]);
