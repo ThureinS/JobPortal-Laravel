28d43dadb0b5cd5e5c33660170ef8c135a7ae2fb
ThureinS <94038134+ThureinS@users.noreply.github.com>
2025-12-30 01:30:12 +0630

    Label component created and refactor the codebase

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -15,12 +15,12 @@
         <form action="{{ route('job-posts.application.store', $jobPost) }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="mb-4">
-                <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
+                <x-label for="expected_salary" :required="true">Expected Salary</x-label>
                 <x-text-input type="number" name="expected_salary" />
             </div>
 
             <div class="mb-4">
-                <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-label :required="true">Upload CV</x-label>
                 <x-text-input type="file" name="cv" />
             </div>
 
